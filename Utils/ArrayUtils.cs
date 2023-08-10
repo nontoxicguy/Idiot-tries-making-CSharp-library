734f85888a950ad74f5ea8a62a9e4ba23e6bbe80
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-08-10 16:59:49 -0400

    The other things I already made

---
+++ Utils/ArrayUtils.cs
@@ -0,0 +1,35 @@
+﻿namespace UtilsCSharp
+{
+    public static class ArrayUtils
+    {
+        /* 3 dimensions? DIY
+         * I refuse to make other functions like that because this library is mostly for copy-pasting.
+         * If this library is in your depedencies, even the simplest app is going to take a bunch of memory.
+         */
+        /// <summary>
+        /// The same as System.Linq's ElementAtOrDefault but for two dimensional arrays.
+        /// </summary>
+        /// <param name="XIndex"></param>
+        /// <param name="YIndex"></param>
+        /// <returns>The object at the spcified index. If it doesn't exist, default.</returns>
+        public static T? ElementAtOrDefault<T>(T[,] array, int XIndex, int YIndex) =>
+            XIndex >= array.GetLowerBound(0) && XIndex < array.GetUpperBound(0) && YIndex >= array.GetLowerBound(1) && YIndex < array.GetUpperBound(1) ? array[XIndex, YIndex] : default;
+
+        /// <summary>
+        /// Adds a value to an array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="valueToAdd"></param>
+        /// <returns>An array but with a new value.</returns>
+        public static T[] Add<T>(T[] array, T valueToAdd)
+        {
+            T[] newArray = new T[array.Length + 1];
+
+            array.CopyTo(newArray, 0);
+            newArray[array.Length] = valueToAdd;
+
+            return newArray;
+        }
+    }
+}
---
+++ Utils/NumberUtils.cs
@@ -0,0 +1,58 @@
+﻿namespace UtilsCSharp
+{
+    public static class NumberUtils
+    {
+        /// <summary>
+        /// Turns number into array of digits.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>A byte array representing the digits of the number.</returns>
+        public static int[] GetDigits(int num)
+        {
+            int[] result = new int[(int)Math.Floor(Math.Log10(num) + 1)];
+
+            for (int i = result.Length - 1; i >= 0; --i)
+            {
+                result[i] = num % 10;
+                num /= 10;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Decomposes the given number into prime numbers.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>An array reprsenting the primes making the given number.</returns>
+        public static int[] PrimeFactors(int num)
+        {
+            List<int> primes = new();
+
+            for (int i = 2; i < num && IsPrime(i); ++i)
+            {
+                while (num % i == 0)
+                {
+                    primes.Add(i);
+                    num /= i;
+                }
+            }
+
+            return primes.ToArray();
+        }
+
+        /// <summary>
+        /// Checks if number is prime.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>True if the given number is prime, else false.</returns>
+        public static bool IsPrime(int num)
+        {
+            for (int i = 2; i < num / 2; ++i)
+                if (num % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
