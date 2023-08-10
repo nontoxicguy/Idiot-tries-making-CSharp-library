96bbb6d358e6e513ab8164a8f7d476dc071657f2
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-08-10 19:20:56 -0400

    added "this" modifier
    I just discovered it.
    Before:
        ArrayUtils.Add(array)
    Now:
        array.Add()

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -12,7 +12,7 @@
         /// <param name="XIndex"></param>
         /// <param name="YIndex"></param>
         /// <returns>The object at the spcified index. If it doesn't exist, default.</returns>
-        public static T? ElementAtOrDefault<T>(T[,] array, int XIndex, int YIndex) =>
+        public static T? ElementAtOrDefault<T>(this T[,] array, int XIndex, int YIndex) =>
             XIndex >= array.GetLowerBound(0) && XIndex < array.GetUpperBound(0) && YIndex >= array.GetLowerBound(1) && YIndex < array.GetUpperBound(1) ? array[XIndex, YIndex] : default;
 
         /// <summary>
@@ -22,7 +22,7 @@
         /// <param name="array"></param>
         /// <param name="valueToAdd"></param>
         /// <returns>An array but with a new value.</returns>
-        public static T[] Add<T>(T[] array, T valueToAdd)
+        public static T[] Add<T>(this T[] array, T valueToAdd)
         {
             T[] newArray = new T[array.Length + 1];
 
