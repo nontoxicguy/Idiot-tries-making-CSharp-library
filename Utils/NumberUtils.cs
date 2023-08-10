1b0bd1d7ef2f888ea998950ead9f41fe84b95561
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-08-10 19:22:16 -0400

    Added "this" modifier

--- Utils/NumberUtils.cs
+++ Utils/NumberUtils.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <param name="num"></param>
         /// <returns>A byte array representing the digits of the number.</returns>
-        public static int[] GetDigits(int num)
+        public static int[] GetDigits(this int num)
         {
             int[] result = new int[(int)Math.Floor(Math.Log10(num) + 1)];
 
@@ -25,7 +25,7 @@
         /// </summary>
         /// <param name="num"></param>
         /// <returns>An array reprsenting the primes making the given number.</returns>
-        public static int[] PrimeFactors(int num)
+        public static int[] PrimeFactors(this int num)
         {
             List<int> primes = new();
 
@@ -46,7 +46,7 @@
         /// </summary>
         /// <param name="num"></param>
         /// <returns>True if the given number is prime, else false.</returns>
-        public static bool IsPrime(int num)
+        public static bool IsPrime(this int num)
         {
             for (int i = 2; i < num / 2; ++i)
                 if (num % i == 0)
