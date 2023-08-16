74659b809601d400528a6b5de23681448823a211
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-08-16 14:14:25 -0400

    Removed ElementAtOrDefault for two dimensional arrays.
    So, I want this library to have some complicated stuff that people can copy and paste to solve their problems. A one liner function is most of the time too simple to solve someone's problem. In this case I think this function is too simple to stay here.

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -2,19 +2,6 @@
 {
     public static class ArrayUtils
     {
-        /* 3 dimensions? DIY
-         * I refuse to make other functions like that because this library is mostly for copy-pasting.
-         * If this library is in your depedencies, even the simplest app is going to take a bunch of memory.
-         */
-        /// <summary>
-        /// The same as System.Linq's ElementAtOrDefault but for two dimensional arrays.
-        /// </summary>
-        /// <param name="XIndex"></param>
-        /// <param name="YIndex"></param>
-        /// <returns>The object at the spcified index. If it doesn't exist, default.</returns>
-        public static T? ElementAtOrDefault<T>(this T[,] array, int XIndex, int YIndex) =>
-            XIndex >= array.GetLowerBound(0) && XIndex < array.GetUpperBound(0) && YIndex >= array.GetLowerBound(1) && YIndex < array.GetUpperBound(1) ? array[XIndex, YIndex] : default;
-
         /// <summary>
         /// Adds a value to an array.
         /// </summary>
