e745916be859de6b69581abadc50e36f2ae58a3a
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-08-30 11:04:18 -0400

    Added Counts
    Counts is basically a function that returns the counts of the items in a dictionary.

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -18,5 +18,26 @@
 
             return newArray;
         }
+
+        /// <summary>
+        /// Gets the count of every value in the array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <returns>A dictionary of the item counts.</returns>
+        public static Dictionary<T, int> Counts<T>(this T[] array) where T : notnull
+        {
+            Dictionary<T, int> result = new();
+    
+            foreach (T item in array)
+            {
+                if (!result.TryAdd(item, 1))
+                {
+                    ++result[item];
+                }
+            }
+    
+            return result;
+        }
     }
 }
