f710aeee2d353044bef6cc58baf6e542f9964bcf
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-09-09 08:37:00 -0400

    Removed Add
    Didn't know LINQ already did that when I wrote the function ¯\_(ツ)_/¯

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -3,23 +3,6 @@
     public static class ArrayUtils
     {
         /// <summary>
-        /// Adds a value to an array.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
-        /// <param name="valueToAdd"></param>
-        /// <returns>An array but with a new value.</returns>
-        public static T[] Add<T>(this T[] array, T valueToAdd)
-        {
-            T[] newArray = new T[array.Length + 1];
-
-            array.CopyTo(newArray, 0);
-            newArray[array.Length] = valueToAdd;
-
-            return newArray;
-        }
-
-        /// <summary>
         /// Gets the count of every value in the array.
         /// </summary>
         /// <typeparam name="T"></typeparam>
