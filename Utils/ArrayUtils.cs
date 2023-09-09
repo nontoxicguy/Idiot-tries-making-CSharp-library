f2bd03cfe40c63f91bda45c9054964deeda6e291
nontoxicguy <140744676+nontoxicguy@users.noreply.github.com>
2023-09-09 11:58:57 -0400

    Added MinGroup function
    New function! The other started to feel lonely :(

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -22,5 +22,45 @@
     
             return result;
         }
+
+        /// <summary>
+        /// Makes a group of the items with the minimum sepcified value
+        /// </summary>
+        /// <typeparam name="TInput"/>
+        /// <typeparam name="TResult"/>
+        /// <param name="input"></param>
+        /// <param name="selector"></param>
+        /// <returns>The items in the enumerable that have the lowest specified value</returns>
+        /// <exception cref="ArgumentNullException"/>
+        public static IEnumerable<TInput> MinGroup<TInput, TResult>(this IEnumerable<TInput> input, Func<TInput, TResult> selector)
+            where TResult : IComparable // If you want to get the lowest values in a number array, remove the selector argument
+        {
+            // Some check, you may not need it.
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+        
+            List<TInput> result = new();
+            TResult min = selector(input.FirstOrDefault()!); // Rename max if you want a MaxGroup function
+        
+            for (int i = 1; i < input.Count(); ++i)
+            {
+                int compared = selector(input.ElementAt(i)).CompareTo(min);
+        
+                // If you want a MaxGroup function replace '<' by '>'
+                if (compared <= 0)
+                {
+                    if (compared < 0)
+                    {
+                        result.Clear();
+                    }
+        
+                    result.Add(input.ElementAt(i));
+                }
+            }
+        
+            return result;
+        }
     }
 }
